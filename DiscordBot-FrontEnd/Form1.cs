ec27d0c503dfed01699180983d7ffcb6350104f1
alexandre_alencar <alexandre.alencar@intelitrader.com.br>
2020-12-27 04:08:38 -0300

    re-adding]

--- DiscordBot-FrontEnd/Form1.cs
+++ DiscordBot-FrontEnd/Form1.cs
@@ -16,5 +16,10 @@ namespace DiscordBot_FrontEnd
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
